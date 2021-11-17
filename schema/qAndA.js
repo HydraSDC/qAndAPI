8455b42fbee6b604d70763ae8c4b5c4311991346
Derek Mason <3derekmason@gmail.com>
2021-11-17 11:53:06 -0700

    connected mongoose to mongoDB instance DB: 'qAndAPI'

---
+++ schema/qAndA.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const QuestionsSchema = new.mongooose.Schema({
+
+})
+
+const Questions = mongoose.model("Questions", QuestionsSchema);
+
+module.exports = Questions;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+const routes = require('./routes.js');
+
 // const { API_URL, API_KEY } = require('./config.js');
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/qAndAPI');
 
 app.listen(3333, () => {
   console.log('Hail Hydra... 3333')
