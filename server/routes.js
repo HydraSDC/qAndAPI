54e3917ad3bc1d780ffe7ec0a68ff751bfac5ab8
Derek Mason <3derekmason@gmail.com>
2021-11-18 11:32:22 -0700

    added GET for answerPhotos and products, will need refactoring obviously

--- server/routes.js
+++ server/routes.js
@@ -27,4 +27,28 @@ app.get("/answers", async (req, res) => {
   })
 })
 
+app.get("/answerPhotos", async (req, res) => {
+  connection.db
+  .collection('answerPhotos')
+  .find({}).limit(10)
+  .toArray((err, data) => {
+    if (err) {res.status(400).send("Error fetching Q's")}
+    else {
+      res.json(data)
+    }
+  })
+})
+
+app.get("/products", async (req, res) => {
+  connection.db
+  .collection('products')
+  .find({}).limit(10)
+  .toArray((err, data) => {
+    if (err) {res.status(400).send("Error fetching Q's")}
+    else {
+      res.json(data)
+    }
+  })
+})
+
 module.exports = app;
