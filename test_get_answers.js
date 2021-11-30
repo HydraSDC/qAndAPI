ffb4a0fe034a2700a839dd1f3ef47864f326cebe
Derek Mason <3derekmason@gmail.com>
2021-11-29 19:28:12 -0700

    fixed k6 test files to run 1000 vus instead of 10

--- test_get_answers.js
+++ test_get_answers.js
@@ -1,6 +1,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  vus: 1000,
+  duration: '30s',
+};
+
 export default function () {
   http.get('http://localhost:3333/qa/answers?question_id=4321');
   sleep(1);
